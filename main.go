0d5c0e155b20d9609932677146b30980a5bfb46f
Luis Martinez <luism568544@gmail.com>
2022-01-07 22:39:55 -0500

    Basic HTML Templating

--- main.go
+++ main.go
@@ -1,16 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"hack-o-ween-site/packages"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+type HTMLData struct {
+	Login    bool
+	Username string
+}
 
-	fmt.Println(packages.Test())
+func HandleRequests(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	dir := "public/"
+
+	log.Println("PATH:", path)
+
+	data := HTMLData{
+		Login:    false,
+		Username: "testificate",
+	}
+
+	// Empty path defaults to index.html
+	if path == "/" {
+		path = "/index"
+	}
+
+	if file := filepath.Join(dir, path); fileExists(file) && filepath.Ext(file) != ".html" { // Serve a File
+		log.Println("Serving File")
+		http.ServeFile(w, r, file)
+	} else if file = filepath.Join(dir, path+".html"); fileExists(file) { // Serve templated HTML
+		log.Println("Serving HTML")
+		tmpl := template.Must(template.ParseFiles(filepath.Join(dir, path+".html")))
+		tmpl.Execute(w, data)
+	}
+
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
+
+func main() {
+	http.HandleFunc("/", HandleRequests)
 
 	http.ListenAndServe(":9956", nil)
 }
--- packages/auth.go
+++ packages/auth.go
@@ -1,11 +1,181 @@
 package packages
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+
+	_ "github.com/mattn/go-sqlite3"
 	_ "golang.org/x/oauth2/github"
 	_ "golang.org/x/oauth2/gitlab"
 	_ "golang.org/x/oauth2/google"
 )
 
-func Test() int {
-	return 6
+const clientID_github = "e3608b8becc8be75c377"
+const clientSecret_github = "edf9f8aea66771b6cd9077325c4c52799cb98710"
+
+type OAuthAccessResponse struct {
+	AccessToken string `json:"access_token"`
+}
+
+func AuthenticationRedirect(w http.ResponseWriter, r *http.Request) {
+	// First, we need to get the value of the `code` query param
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not parse query: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	code := r.FormValue("code")
+
+	// Next, lets for the HTTP request to call the github oauth enpoint
+	// to get our access token
+	reqURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID_github, clientSecret_github, code)
+	req, err := http.NewRequest(http.MethodPost, reqURL, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not create HTTP request: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+	// We set this header since we want the response
+	// as JSON
+	req.Header.Set("accept", "application/json")
+
+	// Send out the HTTP request
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stdout, "could not send HTTP request: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	defer res.Body.Close()
+
+	// Parse the request body into the `OAuthAccessResponse` struct
+	var t OAuthAccessResponse
+	if err := json.NewDecoder(res.Body).Decode(&t); err != nil {
+		fmt.Fprintf(os.Stdout, "could not parse JSON response: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+	}
+
+	// Finally, send a response to redirect the user to the "welcome" page
+	// with the access token
+	w.Header().Set("Location", "/")
+
+	myvar := map[string]interface{}{"Access": []string{"hi", "what's up", "boo"}}
+	outputHTML(w, "public/index.html", myvar)
+
+	w.WriteHeader(http.StatusFound)
+
+	fmt.Println("Login Successful")
+}
+
+func outputHTML(w http.ResponseWriter, filename string, data interface{}) {
+	t, err := template.ParseFiles(filename)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	if err := t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+}
+
+func SQL_Test() {
+	os.Remove("./foo.db")
+
+	db, err := sql.Open("sqlite3", "./foo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStmt := `
+	create table foo (id integer not null primary key, name text);
+	delete from foo;
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStmt)
+		return
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	stmt, err := tx.Prepare("insert into foo(id, name) values(?, ?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	for i := 0; i < 100; i++ {
+		_, err = stmt.Exec(i, fmt.Sprintf("こんにちわ世界%03d", i))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	tx.Commit()
+
+	rows, err := db.Query("select id, name from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err = db.Prepare("select name from foo where id = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+	var name string
+	err = stmt.QueryRow("3").Scan(&name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(name)
+
+	_, err = db.Exec("delete from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = db.Exec("insert into foo(id, name) values(1, 'foo'), (2, 'bar'), (3, 'baz')")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("select id, name from foo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id, name)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
 }
