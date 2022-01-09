535c407db213d034f85aa9907222f1880b5f6899
Luis Martinez <luism568544@gmail.com>
2022-01-08 20:31:19 -0500

    Took out some println calls in countdown.go

--- packages/countdown.go
+++ packages/countdown.go
@@ -1,7 +1,6 @@
 package packages
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 	"time"
@@ -16,14 +15,12 @@ func Get_duration() string {
 	t := time.Now()
 	month := t.Month()
 	day := t.Day()
-	fmt.Println(month, day)
 	if month == START_MONTH && day >= START_DAY && day < START_DAY+ADVENT_DURATION {
 		cd := time.Date(t.Year(), START_MONTH, START_DAY+((day-START_DAY)+1), 0, 0, 0, 0, time.Now().Location())
 		duration_obj := time.Until(cd)
 		hours := duration_obj.Hours()
 		minutes := int(math.Round(duration_obj.Minutes())) % 60
 		time := (strconv.Itoa(int(hours)) + "h " + strconv.Itoa(int(minutes)) + "m")
-		fmt.Println(time)
 		return time
 	} else {
 		return "Thanks for playing!"
