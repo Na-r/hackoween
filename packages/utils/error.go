b6f0ffe19cc13a2f8890d39f984bebb7e5d04c73
Luis Martinez <luism568544@gmail.com>
2022-01-13 20:54:55 -0500

    feat: Add an error checking utility

---
+++ packages/utils/error.go
@@ -0,0 +1,27 @@
+package utils
+
+import "log"
+
+type ErrorLevel uint8
+
+const (
+	Log ErrorLevel = iota
+	Panic
+	Fatal
+)
+
+func CheckErr(err error, error_type ErrorLevel, desc string) {
+	if err != nil {
+		switch error_type {
+		case Log:
+			log.Printf("LOG | Error: %s\n%s", err.Error(), desc)
+
+		case Panic:
+			log.Panicf("PANIC | Error: %s\n%s", err.Error(), desc)
+
+		case Fatal:
+			log.Fatalf("FATAL | Error: %s\n%s", err.Error(), desc)
+
+		}
+	}
+}
